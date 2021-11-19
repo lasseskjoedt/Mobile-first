337108a14ebc334a0a83ead29ed2ea59f3119384
lasseskjoedt <90311426+lasseskjoedt@users.noreply.github.com>
2021-11-19 12:39:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+var items = document.getElementsByName('item');
+var selectedItem = document.getElementById('selected-item');
+var dropdown = document.getElementById('dropdown');
+
+items.forEach(item => {
+  item.addEventListener('change', () => {
+    if (item.checked) {
+      selectedItem.innerHTML = item.value;
+      dropdown.open = false;
+    }
+  });
+});
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
+/* Lykkehjul */
+
+function rotateFunction(){
+  var min = 1024;
+  var max = 9999;
+  var deg = Math.floor(Math.random() * (max - min)) + min;
+  document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+}
